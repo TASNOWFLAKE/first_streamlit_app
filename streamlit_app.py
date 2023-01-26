e76f067de2380f7dd93368b4dcd9b417acc3fd20
TASNOWFLAKE <123203493+TASNOWFLAKE@users.noreply.github.com>
2023-01-26 12:32:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ streamlit.dataframe(my_data_rows)
 
 # New section to display fruit to add
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
